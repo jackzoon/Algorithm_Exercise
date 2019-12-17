122f5c3dcb6c808d52806fcd545876a7ea94daeb
Halen <911452597@qq.com>
2019-12-17 09:57:04 +0800

    用两个栈实现队列

--- src/com/halen/chapter01/TwoStacksQueue.java
+++ src/com/halen/chapter01/TwoStacksQueue.java
@@ -1,10 +1,55 @@
 package com.halen.chapter01;
 
+import java.util.Stack;
+
 /**
  * 用两个栈实现队列
  */
-public class TwoStacksQueue {
+public class TwoStacksQueue<E> {
 
-    
+    private Stack<E> stackPush;
+    private Stack<E> stackPop;
+
+    public TwoStacksQueue() {
+        this.stackPop = new Stack<>();
+        this.stackPush = new Stack<>();
+    }
+
+    public void add(E e) {
+        stackPush.push(e);
+    }
+
+    public E poll() {
+        if (stackPop.isEmpty()) {
+            while (!stackPush.isEmpty()) {
+                stackPop.push(stackPush.pop());
+            }
+        }
+        return stackPop.pop();
+    }
+
+    public E peek() {
+        if (stackPop.isEmpty()) {
+            while (!stackPush.isEmpty()) {
+                stackPop.push(stackPush.pop());
+            }
+        }
+        return stackPop.peek();
+    }
+
+    public boolean isEmpty() {
+        return stackPop.isEmpty() && stackPush.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        TwoStacksQueue<Integer> queue = new TwoStacksQueue();
+
+        queue.add(1);
+        queue.add(2);
+        queue.add(3);
+        while (!queue.isEmpty()) {
+            System.out.println(queue.poll());
+        }
+    }
 
 }
