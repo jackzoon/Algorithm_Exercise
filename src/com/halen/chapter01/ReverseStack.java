b1e4c3245518c7be878684b919699f484d4e4e66
Halen <911452597@qq.com>
2019-12-18 14:26:51 +0800

    逆序一个栈

--- src/com/halen/chapter01/ReverseStack.java
+++ src/com/halen/chapter01/ReverseStack.java
@@ -1,7 +1,63 @@
 package com.halen.chapter01;
 
+import java.util.Stack;
+
 /**
- *
+ * 仅用递归函数和栈操作逆序一个栈
  */
 public class ReverseStack {
+
+    public static int getAndRemoveLastElement(Stack<Integer> stack) {
+        int result = stack.pop();
+        if (stack.isEmpty()) {
+            return result;
+        } else {
+            int last = getAndRemoveLastElement(stack);
+            stack.push(result);
+            return last;
+        }
+    }
+
+    public static void reverse(Stack<Integer> stack) {
+        if (stack.isEmpty()) {
+            return;
+        }
+        int i = getAndRemoveLastElement(stack);
+        reverse(stack);
+        stack.push(i);
+    }
+
+    public static int getAndRemoveLastElement2(Stack<Integer> stack) {
+        int result = stack.pop();
+        if (stack.isEmpty()) {
+            return result;
+        } else {
+            int last = getAndRemoveLastElement2(stack);
+            stack.push(result);
+            return last;
+        }
+    }
+
+    public static void reverse2(Stack<Integer> stack) {
+        if (stack.isEmpty()) {
+            return;
+        }
+        int i = getAndRemoveLastElement2(stack);
+        reverse2(stack);
+        stack.push(i);
+    }
+
+
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        reverse2(stack);
+        while (!stack.isEmpty()) {
+            System.out.println(stack.pop());
+        }
+    }
+
 }
