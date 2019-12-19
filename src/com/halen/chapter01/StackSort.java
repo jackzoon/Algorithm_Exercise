d5a8c3ed0d2d04fb3bcf242a967b3e0246c7e118
Halen <911452597@qq.com>
2019-12-19 17:42:49 +0800

    sortStack

--- src/com/halen/chapter01/StackSort.java
+++ src/com/halen/chapter01/StackSort.java
@@ -1,10 +1,37 @@
 package com.halen.chapter01;
 
+import java.util.Stack;
+
 /**
  * 用一个栈实现另一个栈的排序
  */
 public class StackSort {
 
+    public static void sortStackByStack(Stack<Integer> stack) {
+        Stack<Integer> help = new Stack<>();
+        while (!stack.isEmpty()) {
+            int cur = stack.pop();
+            while (!help.isEmpty() && help.peek() > cur) {
+                stack.push(help.pop());
+            }
+            help.push(cur);
+        }
+        while (!help.isEmpty()) {
+            stack.push(help.pop());
+        }
+    }
 
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        stack.push(5);
+        stack.push(42);
+        stack.push(88);
+        stack.push(13);
+        sortStackByStack(stack);
+        while (!stack.isEmpty()) {
+            System.out.println(stack.pop());
+        }
+    }
 
 }
