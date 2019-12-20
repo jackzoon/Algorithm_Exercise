1043ce7aa4af29068c2cc797221e5d7db4b3eccc
Halen <911452597@qq.com>
2019-12-20 14:56:31 +0800

    maxWindow

--- src/com/halen/chapter01/MaxWindow.java
+++ src/com/halen/chapter01/MaxWindow.java
@@ -1,6 +1,9 @@
 package com.halen.chapter01;
 
 import java.util.Arrays;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
 
 /**
  * 生成窗口最大值数组
@@ -17,6 +20,26 @@ public class MaxWindow {
         return res;
     }
 
+    public static int[] getMaxWindow2(int[] arr, int w) {
+        
+        int[] res = new int[arr.length - w + 1];
+        Deque<Integer> qmax = new LinkedList<>();
+        int index = 0;
+        for (int i = 0; i < arr.length; i++) {
+            while (!qmax.isEmpty() && arr[qmax.peekLast()] <= arr[i]) {
+                qmax.pollLast();
+            }
+            qmax.addLast(i);
+            if (qmax.peekFirst() == i - w) {
+                qmax.pollFirst();
+            }
+            if (i >= w - 1) {
+                res[index++] = arr[qmax.peekFirst()];
+            }
+        }
+        return res;
+    }
+
     public static int getMax(int[] arr, int left, int right) {
         int max = arr[left];
         for (int i = left + 1; i <= right; i++) {
@@ -27,7 +50,7 @@ public class MaxWindow {
 
     public static void main(String[] args) {
         int[] arr = {4, 2, 5, 4, 3, 3, 6, 7};
-        arr = getMaxWindow1(arr, 3);
+        arr = getMaxWindow2(arr, 3);
         System.out.println(Arrays.toString(arr));
     }
 }
