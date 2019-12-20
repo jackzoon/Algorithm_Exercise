7dcc9e6ec9f5e74f879b3c354bf3ffefa33f66be
Halen <911452597@qq.com>
2019-12-20 12:44:22 +0800

    hanoi

--- src/com/halen/chapter01/HanNoi.java
+++ src/com/halen/chapter01/HanNoi.java
@@ -5,21 +5,21 @@ package com.halen.chapter01;
  */
 public class HanNoi {
 
-    public int hanoiProblem1(int num, String left, String mid, String right) {
+    public static int hanoiProblem1(int num, String left, String mid, String right) {
         if (num < 1) {
             return 0;
         }
         return process(num, left, mid, right, left, right);
     }
 
-    public int process(int num, String left, String mid, String right, String from, String to) {
+    public static int process(int num, String left, String mid, String right, String from, String to) {
         if (num == 1) {
             if (from.equals(mid) || to.equals(mid)) {
                 System.out.println("Move 1 from " + from + " to" + to);
                 return 1;
             } else {
-                System.out.println("Move 1 from " + from + " to" + mid);
-                System.out.println("Move 1 from " + mid + " to" + to);
+                System.out.println("Move 1 from " + from + " to " + mid);
+                System.out.println("Move 1 from " + mid + " to " + to);
                 return 2;
             }
         }
@@ -33,13 +33,17 @@ public class HanNoi {
         } else {
             int part1 = process(num - 1, left, mid, right, from, to);
             int part2 = 1;
-            System.out.println("Move " + num + " from " + " to " + mid);
+            System.out.println("Move " + num + " from " + from + " to " + mid);
             int part3 = process(num - 1, left, mid, right, to, from);
             int part4 = 1;
             System.out.println("Move " + num + " from " + mid + " to " + to);
             int part5 = process(num - 1, left, mid, right, from, to);
             return part1 + part2 + part3 + part4 + part5;
         }
+    }
+
+    public static void main(String[] args) {
+        hanoiProblem1(2, "left", "mid", "right");
     }
 
 
